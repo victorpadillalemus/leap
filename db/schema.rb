8d64d0ac0fa6c8b1da154480420c4825006b80aa
Victor Padilla <victor.padilla.lemus@gmail.com>
2019-08-20 13:00:50 +0200

    FEAT: Adding Experience Model to DB Schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_20_102249) do
+ActiveRecord::Schema.define(version: 2019_08_20_105840) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,22 @@ ActiveRecord::Schema.define(version: 2019_08_20_102249) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+  end
+
+  create_table "experiences", force: :cascade do |t|
+    t.text "description"
+    t.integer "capacity"
+    t.integer "price"
+    t.string "category"
+    t.string "title"
+    t.integer "duration"
+    t.string "meeting_point"
+    t.time "start_time"
+    t.time "end_time"
+    t.bigint "airport_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["airport_id"], name: "index_experiences_on_airport_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,4 +58,5 @@ ActiveRecord::Schema.define(version: 2019_08_20_102249) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "experiences", "airports"
 end
