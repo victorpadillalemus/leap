22a81b1dbcf903f027e4a957a4a4c291d01995b7
Victor Padilla <victor.padilla.lemus@gmail.com>
2019-08-20 12:39:54 +0200

    Adding Longitude and Latitude to Airport

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_20_101755) do
+ActiveRecord::Schema.define(version: 2019_08_20_102249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2019_08_20_101755) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
   create_table "users", force: :cascade do |t|
