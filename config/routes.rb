a1066f8a01c5f40e3e8a4bee66146b6308ef7a56
Fahad Alrasheed <fahad.alrasheed@gmail.com>
2019-08-21 11:30:02 +0200

    fixed dates

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only:[:show]
   root to: 'pages#home'
+  get '/filter', to: 'pages#filter', as: 'filter'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2019_08_20_110617) do
-=======
 ActiveRecord::Schema.define(version: 2019_08_20_145406) do
->>>>>>> ee5b823bf201fd0a9af9f4e6889f138a28015e2b
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
