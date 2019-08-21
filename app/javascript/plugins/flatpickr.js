c875bff11f93f7b382f479c4e723b59bce86860c
Victor Padilla <victor.padilla.lemus@gmail.com>
2019-08-21 18:18:48 +0200

    Cards fix

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,8 +1,14 @@
-import flatpickr from "flatpickr"
-import "flatpickr/dist/flatpickr.min.css"
-flatpickr(".datepicker", {
-flatpickr(".datepicker", {
-  altInput: true,
-  minDate: "tomorrow"
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+const datePicker = document.querySelector(".datepicker");
+
+if (datePicker) {
+  flatpickr(".datepicker", {
+    altInput: true,
+    minDate: "tomorrow",
+    disableMobile: "false"
+  });
+}
 
 
