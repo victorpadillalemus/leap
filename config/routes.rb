4003fefae85c110cfeb21b9043ad861eec7d8026
Zach Alvstad <learnwithzach@gmail.com>
2019-08-21 14:19:52 +0200

    Created NavBar and User Profile

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   resources :experiences
   devise_for :users
-  resources :users, only:[:show]
+  resources :users, only: :show
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
