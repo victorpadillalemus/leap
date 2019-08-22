dfad275645796a5c035c11aea65a47022b25fcb0
Zach Alvstad <learnwithzach@gmail.com>
2019-08-22 12:51:52 +0200

    Made small changes to vertical scrolling card container as well as show page of each experience, now going to start adding experience pictures from cloudinary

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def home_page?
     params[:controller] == 'pages' && params[:action] == 'home'
   end
+
+  def nav_transparent?
+    params[:controller] == 'pages' && params[:action] == 'show'
+  end
 end
