0b856ff4ae2e6da5433fa8ef0e8563f52f65b25c
Fahad Alrasheed <fahad.alrasheed@gmail.com>
2019-08-22 14:14:25 +0200

    sliderlogic

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,14 +1,28 @@
 import "bootstrap";
 import "../plugins/flatpickr";
 
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
+var slider = document.getElementById("myRange")
+slider.max = "49";
+var output = document.getElementById("search_arrival");
 output.innerHTML = slider.value; // Display the default slider value
+
+var slider2 = document.getElementById("myRange2");
+slider2.max = "49";
+var output2 = document.getElementById("search_departure");
+output2.innerHTML = slider2.value;
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
-  output.innerHTML = this.value;
+  const hour= (this.value - 1) / 2
+  output.value = `${(Math.floor(hour) + 8) % 24} : ${Number.isInteger(hour) ? "00" : "30" }`
+
 }
+
+slider2.oninput = function() {
+  const hour= (this.value -1) / 2
+  output2.value = `${(Math.floor(hour) + 8) % 24} :${Number.isInteger(hour) ? "00" : "30"}`
+}
+
 
 
 $(document).ready(function() {
