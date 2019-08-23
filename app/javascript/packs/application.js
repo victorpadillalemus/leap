95898fc206acfa783e91453b2b3cd1da03a18074
Ferdinand Croy <af.croy@yahoo.de>
2019-08-23 14:54:24 +0200

    back button implemented

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,12 @@ import { navSlide } from "../components/navslide";
 import { inputSlider } from "../components/form";
 
 
+const backButton = document.querySelector(".backjs")
+
+if (backButton)
+{
+  backButton.addEventListener("click", () => history.back())
+}
 
 navSlide();
 inputSlider();
