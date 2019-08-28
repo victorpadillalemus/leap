6e44fafcbbc1902279bbdc86efd3a1a33cca9919
Victor Padilla <victor.padilla.lemus@gmail.com>
2019-08-28 17:47:57 +0200

    FIX

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,16 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  after_create :send_welcome_email
+  # after_create :send_welcome_email
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :favorites, dependent: :destroy
   has_many :experiences, through: :favorites
   has_many :bookings, dependent: :destroy
 
-  def send_welcome_email
-    UserMailer.with(user: self).welcome.deliver_now
-  end
+  # def send_welcome_email
+  #   UserMailer.with(user: self).welcome.deliver_now
+  # end
 
   def favorited?(experience)
     favorites.find_by(experience: experience)
--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -2,9 +2,9 @@
 class UserMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/welcome
-  def welcome
-    user = User.first
-    UserMailer.with(user: user).welcome
-  end
+  # def welcome
+  #   user = User.first
+  #   UserMailer.with(user: user).welcome
+  # end
 
 end
