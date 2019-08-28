b7254adc0e7f64e3885e7586a91f90791f7455bc
Fahad Alrasheed <fahad.alrasheed@gmail.com>
2019-08-28 09:10:14 +0200

    timecounter done

--- app/javascript/components/form.js
+++ app/javascript/components/form.js
@@ -2,6 +2,7 @@ const inputSlider = () => {
   const slider = document.getElementById("myRange");
 
   if (slider) {
+    slider.max = "49";
     const output = document.getElementById("search_arrive");
     output.innerHTML = slider.value; // Display the default slider value
 
@@ -9,16 +10,25 @@ const inputSlider = () => {
   slider2.max = "49";
   var output2 = document.getElementById("search_depart");
   output2.innerHTML = slider2.value;
+  const transit = document.querySelector(".inject-transit-time")
 
   // Update the current slider value (each time you drag the slider handle)
     slider.oninput = function() {
-      const hour= (this.value -1) / 2
-      output.value = `${(Math.floor(hour) + 0) % 24} :${Number.isInteger(hour) ? "00" : "30"}`
+      const aHour= (this.value -1) / 2
+      output.value = `${(Math.floor(aHour) + 0) % 24} :${Number.isInteger(aHour) ? "00" : "30"}`
+      const dHour= (slider2.value -1) / 2
+      const time = dHour - aHour
+      const transitTime = `${(Math.floor(time) + 0) % 24} :${Number.isInteger(time) ? "00" : "30"}`
+      transit.innerHTML = transitTime
     }
 
   slider2.oninput = function() {
-    const hour= (this.value -1) / 2
-    output2.value = `${(Math.floor(hour) + 24) % 24} :${Number.isInteger(hour) ? "00" : "30"}`
+    const dHour= (this.value -1) / 2
+    output2.value = `${(Math.floor(dHour) + 24) % 24} :${Number.isInteger(dHour) ? "00" : "30"}`
+    const aHour= (slider.value -1) / 2
+    const time = dHour - aHour
+    const transitTime = `${(Math.floor(time) + 0) % 24} :${Number.isInteger(time) ? "00" : "30"}`
+    transit.innerHTML = transitTime
   }
 
     $(document).ready(function() {
@@ -40,5 +50,14 @@ const inputSlider = () => {
 
   }
 }
+
+// const valuestart = $("select[name='timestart']").val();
+// const valuestop = $("select[name='timestop']").val();
+
+// //create date format
+// var timeStart = new Date("01/01/2007 " + valuestart).getHours();
+// var timeEnd = new Date("01/01/2007 " + valuestop).getHours();
+
+// var hourDiff = timeEnd - timeStart;
 
 export { inputSlider }
